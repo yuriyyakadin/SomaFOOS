94ec165eac9bb0f4f9d5c4faf68895fbc8504dd9
yuriyyakadin <yuriyysergeevich@rambler.ru>
2026-06-26 07:31:13 +0300

    Revrote a bootloader from scratch

--- SomaBootManager/boot1.asm
+++ SomaBootManager/boot1.asm
@@ -1,13 +1,11 @@
-[bits 16]
 [org 0x7c00]
-global _start
-_start:
-    ;setting up stack
+[bits 16]
+start:
     cli
     xor ax, ax
-    mov sp, ax
+    mov ss, ax
+    mov sp, 0x7c00
     sti
-    ;clearing all registers
     mov ds, ax
     mov fs, ax
     mov gs, ax
@@ -15,30 +13,47 @@ _start:
     mov bx, ax
     mov cx, ax
     mov dx, ax
-    mov di, ax
     mov si, ax
+    mov di, ax
     mov bp, ax
-    jmp read_disk
-    ;reading second sector of the disk
-read_disk:
-    mov ah, 0x02
-    mov al, 0x01
-    mov ch, 0x00
-    mov cl, 0x02
-    mov dh, 0x00
+    jmp reset_disk
+reset_disk:
+    mov si, msg_start_reset
+    call print_string
+    mov ah, 0
     mov dl, 0x80
-    mov bx, 0x7e00
-    int 0x13 
-    jc read_disk_error
-    jmp 0x0000:0x7e00
-read_disk_error:
+    int 0x13
+    jc reset_disk
+    mov si, msg_reset_disk
+    call print_string
+    jmp read_second_sector
+read_second_sector:
+    mov si, msg_start_read
+    call print_string
+    xor ax, ax
+    mov ah, 0x02
+    mov al, 4 
+    mov ch, 0
+    mov dh, 0
+    mov cl, 2
+    mov dl, 0x80
+    mov bx, 0x8000
+    int 0x13
+    jc read_second_sector
+    jmp 0x0000:0x8000
+print_string:
     xor ax, ax
     mov ah, 0x0e
-    mov al, 'N'
+.print_char:
+    lodsb
+    cmp al, 0
+    je .done
     int 0x10
-    jmp stuck
-stuck:
-    hlt
-    jmp stuck
+    jmp .print_char
+.done:
+    ret
+msg_start_reset db "Starting disk reset...", 13, 10, 0
+msg_reset_disk db "Disk reset completed successfully.", 13, 10, 0
+msg_start_read db "Starting reading second sector of the disk...", 13, 10, 0
 times 510 - ($ - $$) db 0
 dw 0xAA55
--- SomaBootManager/boot2.asm
+++ SomaBootManager/boot2.asm
@@ -1,21 +1,32 @@
 [bits 16]
-[org 0]
-global _start1
-_start1:
-    xor ax, ax
-    mov ah, 0x0e
-    mov al, 'Y'
-    int 0x10
-    mov al, 0x0d
-    int 0x10
-    mov al, 0x0a
-    int 0x10
-    ;check a20 line
-    call check_a20
+[org 0x8000]
+start:
+    mov si, msg_readed
+    call print_string
+    mov si, msg_starting_cpuid
+    call print_string
     call cpuid
     call using_cpuid
-    call checking_presence_of_msrs
+    call check_msr
+    mov si, msg_cpuid_available
+    call print_string
+    mov si, msg_starting_A20
+    call print_string
+    call check_a20
+    mov si, msg_a20_enabled
+    call print_string
     jmp stuck
+print_string:
+    xor ax, ax
+    mov ah, 0x0e
+.print_char:
+    lodsb
+    cmp al, 0
+    je .done
+    int 0x10
+    jmp .print_char
+.done:
+    ret
 cpuid:
     pushfd
     pushfd
@@ -26,137 +37,23 @@ cpuid:
     xor eax, [esp]
     popfd
     and eax, 0x00200000
-    jz .cpuid_notavaliable
-    jnz .cpuid_avaliable
-.cpuid_notavaliable:
-    xor ax, ax
-    mov ah, 0x0e
-    mov al, 'C'
-    int 0x10 
-    mov al, 'P'
-    int 0x10 
-    mov al, 'U'
-    int 0x10 
-    mov al, 'B'
-    int 0x10
-    mov al, 0x0d
-    int 0x10
-    mov al, 0x0a
-    int 0x10
+    jz .cpuid_error
+    jnz .cpuid_available
     ret
-.cpuid_avaliable:
-    xor ax, ax
-    mov ah, 0x0e
-    mov al, 'C'
-    int 0x10
-    mov al, 'P'
-    int 0x10
-    mov al, 'U'
-    int 0x10
-    mov al, 'A'
-    int 0x10
-    mov al, 0x0d
-    int 0x10
-    mov al, 0x0a
-    int 0x10
-    ret
-check_a20:
-    pushf
-    push ds
-    push es
-    push di
-    push si
-    cli
-    xor ax, ax
-    mov es, ax
-    not ax
-    mov ds, ax
-    mov di, 0x0500
-    mov si, 0x0510
-    mov al, byte [es:di]
-    push ax
-    mov al, byte [ds:si]
-    push ax
-    mov byte [es:di], 0x00
-    mov byte [ds:si], 0xFF
-    cmp byte [es:di], 0xFF
-    pop ax
-    mov byte [ds:si], al
-    pop ax
-    mov byte [es:di], al
-    mov ax, 0
-    je .check_a20_notcorrect
-    jne .check_a20_correct
-    mov ax, 1
-.check_a20_correct:
-    xor ax, ax
-    mov ah, 0x0e
-    mov al, 'A'
-    int 0x10
-    mov al, '2'
-    int 0x10
-    mov al, '0'
-    int 0x10
-    mov al, 'W'
-    int 0x10
-    mov al, 'E'
-    int 0x10
-    mov al, 0x0d
-    int 0x10
-    mov al, 0x0a
-    int 0x10
-    pop si
-    pop di
-    pop es
-    pop ds
-    popf
-    ret
-.check_a20_notcorrect:
-    in al, 0x92
-    or al, 2
-    out 0x92, al
-    xor ax, ax
-    mov ah, 0x0e
-    mov al, 'A'
-    int 0x10
-    mov al, '2'
-    int 0x10
-    mov al, '0'
-    int 0x10
-    mov al, 'E'
-    int 0x10
-    mov al, 0x0d
-    int 0x10
-    mov al, 0x0a
-    int 0x10
-    pop si
-    pop di
-    pop es
-    pop ds
-    popf
+.cpuid_error:
+    mov si, msg_cpuid_error
+    call print_string
+    jmp stuck
+.cpuid_available:
     ret
 using_cpuid:
     mov eax, 0
     cpuid
+    cmp ebx, 0x756E6547
+    je .intelp
     cmp ebx, 0x68747541
     je .amdp
-    cmp ebx, 0x756e6547
-    je .intelp
     jne .unknownp
-.amdp:
-    xor ax, ax
-    mov ah, 0x0e
-    mov al, 'A'
-    int 0x10
-    mov al, 'M'
-    int 0x10
-    mov al, 'D'
-    int 0x10
-    mov al, 0x0d
-    int 0x10
-    mov al, 0x0a
-    int 0x10
-    ret
 .unknownp:
     xor ax, ax
     mov ah, 0x0e
@@ -165,6 +62,20 @@ using_cpuid:
     mov al, 'N'
     int 0x10
     mov al, 'K'
+    int 0x10
+    mov al, 0x0d
+    int 0x10
+    mov al, 0x0a
+    int 0x10
+    ret
+.amdp:
+    xor ax, ax
+    mov ah, 0x0e
+    mov al, 'A'
+    int 0x10
+    mov al, 'M'
+    int 0x10
+    mov al, 'D'
     int 0x10
     mov al, 0x0d
     int 0x10
@@ -185,44 +96,69 @@ using_cpuid:
     mov al, 0x0a
     int 0x10
     ret
-checking_presence_of_msrs:
+check_msr:
     mov eax, 1
     cpuid
-    and edx, 32
-    jz .no_msr
-    jnz .yes_msr
-.no_msr:
-    xor ax,ax
-    mov ah, 0x0e
-    mov al, 'N'
-    int 0x10
-    mov al, 'M'
-    int 0x10
-    mov al, 'S'
-    int 0x10
-    mov al, 'R'
-    int 0x10
-    mov al, 0x0d
-    int 0x10
-    mov al, 0x0a
-    int 0x10
+    test edx, 1 << 5
+    jz .msr_no
+    jnz .msr_yes
+.msr_no:
+    mov si, msg_msr_no
+    call print_string
+    jmp stuck
+.msr_yes:
     ret
-.yes_msr:
-    xor ax,ax
-    mov ah, 0x0e
-    mov al, 'Y'
-    int 0x10
-    mov al, 'M'
-    int 0x10
-    mov al, 'S'
-    int 0x10
-    mov al, 'R'
-    int 0x10
-    mov al, 0x0d
-    int 0x10
-    mov al, 0x0a
-    int 0x10
+check_a20:
+    pushf
+    push ds
+    push es
+    push di
+    push si
+    cli
+    xor ax, ax
+    mov es, ax
+    not ax
+    mov ds, ax
+    mov di, 0x0500
+    mov si, 0x0510
+    mov al, byte [es:si]
+    push ax
+    mov al, byte [ds:si]
+    push ax
+    mov byte [es:di], 0x00
+    mov byte [ds:si], 0xFF
+    cmp byte [es:di], 0xFF
+    pop ax
+    mov byte [ds:si], al
+    pop ax
+    mov byte [es:di], al
+    mov ax, 0
+    je .enable_a20
+    mov ax, 1
+    je .a20_enabled
+.a20_enabled:
+    popf 
+    pop ds
+    pop es
+    pop di
+    pop si
+    ret
+.enable_a20:
+    mov ax, 0x2401
+    int 0x15
+    popf
+    pop ds
+    pop es
+    pop di
+    pop si
     ret
 stuck:
     hlt
     jmp stuck
+msg_readed db "Readed second sector of the disk successfully.", 13, 10,  0
+msg_starting_cpuid db "Starting CPUID and MSR-registers availability check...", 13, 10, 0
+msg_cpuid_error db "CPUID availability check failed.", 13, 10, 0
+msg_msr_no db "MSR-registers availability check failed.", 13, 10, 0
+msg_cpuid_available db "CPUID and MSR-registers availability check completed successfully.", 13, 10, 0
+msg_starting_A20 db "Starting A20 line status check...", 13, 10, 0
+msg_a20_enabled db "A20 line status: enabled.", 13, 10, 0
